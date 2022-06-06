91f1935319eb5ba17ea62dcc27fef62c3658e8b1
Nick Pisano <pisano.nicholas@yahoo.com>
2022-06-06 02:40:10 -0700

    Added Win and losegamestate

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,47 @@
-
 // Time Variables
 var timeEl = document.getElementById("Timer");
 var timerCount = 30;
 // Time function
 function timerStart() {
   console.log("timerStart function has started")
-  timerInterval = setInterval(function(){
-    timerCount --;
+  timerInterval = setInterval(function () {
+    timerCount--;
     timeEl.textContent = timerCount + " Seconds Left";
-    if(timerCount <= 0 || currentQuestionIndex >= questionAnswerContainer.length){
-      startButton.setAttribute('style', 'display: block')
+    if (timerCount <= 0 || currentQuestionIndex >= questionAnswerContainer.length) {
+      LoseGame();
       clearInterval(timerInterval);
-    }  },1000);
-  }
+    }
+  }, 1000);
+}
 
-  
-  var startButton = document.getElementById("startButton");
-  var myQuestion  = document.getElementById("questionContainer");
-  var currentQuestion = document.getElementById("currentQuestion");
-  var buttonContainer = document.querySelector("#buttonContainer");
 
-  startButton.addEventListener("click", gameStart);
+var startButton = document.getElementById("startButton");
+var myQuestion = document.getElementById("questionContainer");
+var currentQuestion = document.getElementById("currentQuestion");
+var buttonContainer = document.querySelector("#buttonContainer");
 
-  let switchQuestions, currentQuestionIndex;
+startButton.addEventListener("click", gameStart);
 
-  function gameStart() {
-    timerCount = 30;
-    console.log("startGame Function has started")
-    startButton.setAttribute('style', 'display: none')
-    switchQuestions = questionAnswerContainer.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0;
-    timerStart();
-    questionShow();
-  }
+let switchQuestions, currentQuestionIndex;
+
+function gameStart() {
+  timerCount = 30;
+  console.log("startGame Function has started")
+  startButton.setAttribute('style', 'display: none')
+  switchQuestions = questionAnswerContainer.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0;
+  timerStart();
+  questionShow();
+}
 
 function questionShow() {
   currentQuestion.innerText = questionAnswerContainer[currentQuestionIndex].question;
   var answerButDisplay = document.getElementsByClassName('paBtn');
   console.log(answerButDisplay);
-  for(i=0; i<answerButDisplay.length; i++){
-      answerButDisplay[i].textContent = questionAnswerContainer[currentQuestionIndex].answers[i].text;
-      answerButDisplay[i].setAttribute("data-correct", questionAnswerContainer[currentQuestionIndex].answers[i].correct)
-      answerButDisplay[i].addEventListener("click", checkAnswer)
+  for (i = 0; i < answerButDisplay.length; i++) {
+    answerButDisplay[i].textContent = questionAnswerContainer[currentQuestionIndex].answers[i].text;
+    answerButDisplay[i].setAttribute("data-correct", questionAnswerContainer[currentQuestionIndex].answers[i].correct)
+    answerButDisplay[i].addEventListener("click", checkAnswer)
   }
 }
 
@@ -50,79 +50,103 @@ function checkAnswer(e) {
   var checkAnswerIncorrect = document.getElementById('checkAnswerIncorrect')
   var checkAnswerTime = 2;
   var userChoice = e.target.dataset.correct;
-  if(userChoice == "true"){
-      console.log('yes')
-      checkAnswerCorrect.setAttribute('style', 'display: block')
-      checkAnswerInterval = setInterval(function(){
-        checkAnswerTime --;
-        if(checkAnswerTime <= 0){
-          checkAnswerCorrect.setAttribute('style', 'display: none')
-          clearInterval(checkAnswerInterval);
-        }  },1000);
-
-  }else {
-      console.log("bad")
-      timerCount -=10;
-      checkAnswerIncorrect.setAttribute('style', 'display: block')
-      checkAnswerInterval = setInterval(function(){
-        checkAnswerTime --;
-        if(checkAnswerTime <= 0){
-          checkAnswerIncorrect.setAttribute('style', 'display: none')
-          clearInterval(checkAnswerInterval);
-        }  },1000);
+  if (userChoice == "true") {
+    console.log('yes')
+    checkAnswerCorrect.setAttribute('style', 'display: block')
+    checkAnswerInterval = setInterval(function () {
+      checkAnswerTime--;
+      if (checkAnswerTime <= 0) {
+        checkAnswerCorrect.setAttribute('style', 'display: none')
+        clearInterval(checkAnswerInterval);
+      }
+    }, 1000);
+    
+  } else {
+    console.log("bad")
+    timerCount -= 10;
+    checkAnswerIncorrect.setAttribute('style', 'display: block')
+    checkAnswerInterval = setInterval(function () {
+      checkAnswerTime--;
+      if (checkAnswerTime <= 0) {
+        checkAnswerIncorrect.setAttribute('style', 'display: none')
+        clearInterval(checkAnswerInterval);
+      }
+    }, 1000);
   }
   currentQuestionIndex++;
-
-  if(currentQuestionIndex<questionAnswerContainer.length) {
-      questionShow()
-  } 
+  
+  if (currentQuestionIndex < questionAnswerContainer.length) {
+    questionShow()
+  } else {
+    WinGame();
+  }
 }
 
-        var questionAnswerContainer =[
-          {
-              question:"What native country is Brazil?",
-                  answers: [
-                      {text: "North American", correct:true},
-                      {text: "South American", correct:false},
-                      {text: "West American", correct:false},
-                      {text: "East American", correct:false}
-                  ]
-              },
-              {
-                question:"What is the national language of Canada?",
-                    answers: [
-                        {text: "English", correct:false},
-                        {text: "French", correct:false},
-                        {text: "German", correct:false},
-                        {text: "Dutch", correct:true}
-                    ]
-              },
-              {
-                question:"A la Crecy is a French dish made of what?",
-                    answers: [
-                        {text: "Apples", correct:false},
-                        {text: "Carrots", correct:true},
-                        {text: "Potatoes", correct:false},
-                        {text: "Tomatos", correct:false}
-                    ]
-              },
-              {
-                  question:"What does Nan stand for?",
-                      answers: [
-                          {text: "Neither a Number", correct:false},
-                          {text: "no and no", correct:false},
-                          {text: "Not a Nose", correct:false},
-                          {text: "Not a Number", correct:true}
-                      ]
-              },
-              {
-                  question:"Which core ingredient is important to cook a savory dish?",
-                      answers: [
-                          {text: "Sugar", correct:false},
-                          {text: "Butter", correct:false},
-                          {text: "Salt", correct:true},
-                          {text: "Meat", correct:false}
-                      ]
-                  }
-              ]
-        
+function WinGame() {
+  // var numChars = prompt("How many character ")
+      var userInitials = prompt("Want to save your highscore, enter 3 characters");
+      
+      // check the users input and confirm they put in a number between 8 and 128
+      if (userInitials.length != 3) {
+        alert("Too many characters or not enough characters. Please only type 3 characters!")
+        WinGame();
+        return;
+      }
+      alert("Highscore Saved!")
+
+    }
+    
+    function LoseGame() {
+      alert("You lost. Press the start button to try again!")
+      startButton.setAttribute('style', 'display: block')
+    }
+    
+    
+    var questionAnswerContainer = [
+      {
+        question: "What native country is Brazil?",
+        answers: [
+      { text: "North American", correct: true },
+      { text: "South American", correct: false },
+      { text: "West American", correct: false },
+      { text: "East American", correct: false }
+    ]
+  },
+  {
+    question: "What is the national language of Canada?",
+    answers: [
+      { text: "English", correct: false },
+      { text: "French", correct: false },
+      { text: "German", correct: false },
+      { text: "Dutch", correct: true }
+    ]
+  },
+  {
+    question: "A la Crecy is a French dish made of what?",
+    answers: [
+      { text: "Apples", correct: false },
+      { text: "Carrots", correct: true },
+      { text: "Potatoes", correct: false },
+      { text: "Tomatos", correct: false }
+    ]
+  },
+  {
+    question: "What does Nan stand for?",
+    answers: [
+      { text: "Neither a Number", correct: false },
+      { text: "no and no", correct: false },
+      { text: "Not a Nose", correct: false },
+      { text: "Not a Number", correct: true }
+    ]
+  },
+  {
+    question: "Which core ingredient is important to cook a savory dish?",
+    answers: [
+      { text: "Sugar", correct: false },
+      { text: "Butter", correct: false },
+      { text: "Salt", correct: true },
+      { text: "Meat", correct: false }
+    ]
+  }
+]
+
